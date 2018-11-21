bfe709995f4ad44bd64bf7dd6cf274bfba1eb723
Matheus Henrique <mathsoares1@gmail.com>
2018-11-21 17:03:58 -0300

    Lista de cards é populada por requisição; separado componente de lista de cards

--- src/app/card/card-deck/card-deck.page.ts
+++ src/app/card/card-deck/card-deck.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CardService } from '../shared/card.service';
+import { CardDeck } from '../shared/card.model';
 
 @Component({
     selector: 'app-card-deck',
@@ -7,17 +8,31 @@ import { CardService } from '../shared/card.service';
     styleUrls: [ './card-deck.page.scss' ]
 })
 export class CardDeckPage {
+    private readonly ALLOWED_DECKS = [
+        'classes',
+        'factions',
+        'qualities',
+        'types',
+        'races'
+    ];
     
-    public cardDecks: string[] = [];
+    public cardDecks: CardDeck[] = [];
 
     constructor(private cardService: CardService) {
         this.getCardDecks();
     }
 
     private getCardDecks() {
-        debugger;
         this.cardService.getAllCardDecks().subscribe(
-            (cardDecks: string[]) => this.cardDecks = cardDecks
+            (cardDecks: CardDeck[]) => {
+                this.extractAllowedDecks(cardDecks);
+            }
         );
+    }
+
+    extractAllowedDecks(cardDecks: CardDeck[]) {
+        this.ALLOWED_DECKS.forEach((deckName: string) => {
+            this.cardDecks.push({name: deckName, types: cardDecks[deckName]})
+        })
     }
 }
--- src/app/card/shared/card.service.ts
+++ src/app/card/shared/card.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
 import { of as ObservableOf, Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { CardDeck } from './card.model';
 
 @Injectable()
 export class CardService {
+    private readonly HS_API_URL = 'https://omgvamp-hearthstone-v1.p.mashape.com';
+    private readonly API_KEY = '8k0vwaCkVEmshAUSoKv786h3SHRZp1zYbhsjsnU6peQbZMIEjE';
     
     private readonly cardDecks: string[] = [
         'Druid',
@@ -16,7 +21,13 @@ export class CardService {
         'Paladin'
     ];
 
-    public getAllCardDecks(): Observable<string[]> {
-        return ObservableOf(this.cardDecks);
+    constructor(private http: HttpClient) {
+
+    }
+
+    public getAllCardDecks(): Observable<CardDeck[]> {
+        const headers = new HttpHeaders({'X-Mashape-Key': this.API_KEY});
+
+        return this.http.get<CardDeck[]>(`${this.HS_API_URL}/info`, {headers});
     }
 }
