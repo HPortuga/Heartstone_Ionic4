e4091e33a56c2d84f40c1680c18b2b005ceedb00
Matheus Henrique <mathsoares1@gmail.com>
2018-12-05 10:33:57 -0300

    Criado loader

--- src/app/card/card-detail/card-detail.page.ts
+++ src/app/card/card-detail/card-detail.page.ts
@@ -1,3 +1,4 @@
+import { LoadingController } from '@ionic/angular';
 import { Card } from './../shared/card.model';
 import { CardService } from './../shared/card.service';
 import { ActivatedRoute } from '@angular/router';
@@ -10,22 +11,38 @@ import { Component } from '@angular/core';
 })
 export class CardDetailPage {
     card: Card;
+    loader: any;
 
     constructor(private route: ActivatedRoute,
-                private cardService: CardService) {}
+        private cardService: CardService,
+        private loadingCtrl: LoadingController) { }
 
-    ionViewWillEnter(){
+    async ionViewWillEnter() {
         const cardId = this.route.snapshot.paramMap.get('cardId');
+
+        this.loader = await this.presentLoading();
         this.cardService.getCardById(cardId)
             .subscribe((card: Card[]) => {
                 this.card = card.map((card: Card) => {
                     card.text = this.cardService.replaceCardTextLine(card.text);
                     return card;
                 })[0];
+
+                this.loader.dismiss();
             });
     }
 
     updateImage() {
         this.card.img = 'assets/image/DefaultCard.png';
+    }
+
+    private async presentLoading() {
+        const loader = await this.loadingCtrl.create({
+            message: 'Loading...',
+            translucent: true
+        });
+        loader.present();
+
+        return loader;
     }
 }
--- src/app/card/card-listing/card-listing.page.ts
+++ src/app/card/card-listing/card-listing.page.ts
@@ -1,6 +1,7 @@
 import { CardService } from './../shared/card.service';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
 import { Card } from '../shared/card.model';
 
@@ -13,20 +14,35 @@ export class CardListingPage {
   cardDeckGroup: string;
   cardDeck: string;
   cards: Card[] = [];
+  loader: any;
 
   constructor(private route: ActivatedRoute,
-              private cardService: CardService) { }
+    private cardService: CardService,
+    private loadingCtrl: LoadingController) { }
 
-  ionViewWillEnter(){
+  async ionViewWillEnter() {
     this.cardDeckGroup = this.route.snapshot.paramMap.get('cardDeckGroup');
     this.cardDeck = this.route.snapshot.paramMap.get('cardDeck');
 
+    this.loader = await this.presentLoading();
     this.cardService.getCardsByDeck(this.cardDeckGroup, this.cardDeck)
       .subscribe((cards) => {
-          this.cards = cards.map((card: Card) => {
-            card.text = this.cardService.replaceCardTextLine(card.text);
-            return card;
+        this.cards = cards.map((card: Card) => {
+          card.text = this.cardService.replaceCardTextLine(card.text);
+          return card;
         });
+        
+        this.loader.dismiss();
       });
+  }
+
+  private async presentLoading() {
+    const loader = await this.loadingCtrl.create({
+      message: 'Loading...',
+      translucent: true
+    });
+    loader.present();
+
+    return loader;
   }
 }
