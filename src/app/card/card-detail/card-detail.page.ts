c11993ce92f241b9783b49a8d3363c2a3b5b7dcc
Matheus Henrique <mathsoares1@gmail.com>
2018-12-05 10:15:24 -0300

    alterado css de card-detail

--- src/app/card/card-detail/card-detail.page.ts
+++ src/app/card/card-detail/card-detail.page.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-card-detail',
     templateUrl: './card-detail.page.html',
+    styleUrls: ['./card-detail.page.scss']
 })
 export class CardDetailPage {
     card: Card;
